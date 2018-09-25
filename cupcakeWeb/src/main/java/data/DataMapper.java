1af5b4f676ae96e2f0bb6f7d00a1154905673469
Mark <mark.d.denner@gmail.com>
2018-09-25 12:36:38 +0200

    datamapper

--- cupcakeWeb/src/main/java/data/DataMapper.java
+++ cupcakeWeb/src/main/java/data/DataMapper.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
-
-/**
- *
- * @author Mark
- */
 public class DataMapper {
+    
+    /*
+    Methods:
+    getUsers;
+    getUser(int userID);
+    getOrderAndDetails(int userID);
+    getAllOrdersFromUser(int userID);
+    getAllOrders();
+    getItemTops
+    getItemBottoms
+    getItemTopPrice
+    getItemBottomPrice
+    
+    --------------
+    addUser(User user);
+    addOrder(Order order);
+    
+    
+    */
+    
+    private final String GET_ALL_USERS = "";
+    private final String GET_USER_BY_ID = "";
+    private final String GET_ORDER_AND_DETAIL = "";
+    private final String GET_ALL_ORDERS_FROM_USER = "";
+    private final String GET_ALL_ORDERS = "";
+    private final String GET_TOP_BY_ID = "";
+    private final String GET_BOTTOM_BY_ID = "";
+    private final String GET_ALL_TOPS = "";
+    private final String GET_ALL_BOTTOMS = "";
+    private final String GET_TOP_PRICE = "";
+    private final String GET_BOTTOM_PRICE = "";
+    
+    
     
 }
